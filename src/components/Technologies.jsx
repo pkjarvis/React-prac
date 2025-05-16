46c140f62804ae97d3fc26c4eef55d44c0fa4dd0
Dell <singh.pankaj@jarvis.consulting>
2025-05-16 12:42:18 +0530

    finished

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,44 +1,111 @@
-import React from 'react'
-import { BiLogoPostgresql } from 'react-icons/bi'
-import { DiRedis } from 'react-icons/di'
-import { FaDocker, FaGitAlt, FaNode, FaNodeJs } from 'react-icons/fa'
-import { RiReactjsLine, RiTailwindCssFill } from 'react-icons/ri'
-import { SiMongodb } from 'react-icons/si'
-import { TbBrandNextjs } from 'react-icons/tb'
+import React from "react";
+import { BiLogoPostgresql } from "react-icons/bi";
+import { DiRedis } from "react-icons/di";
+import { FaDocker, FaGitAlt, FaNode, FaNodeJs } from "react-icons/fa";
+import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
+import { SiMongodb } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
+import { animate, motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const Technologies = () => {
   return (
-    <div className='pb-24'>
-      <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div>
-            <RiReactjsLine className='text-7xl text-cyan-400' />
-        </div>
-        <div className='p-4'>
-            <TbBrandNextjs className='text-7xl' />
-        </div>
-        <div className='p-4'>
-            <SiMongodb className='text-7xl text-green-800' />
-        </div>
-        <div className='p-4'>
-            <FaNodeJs className='text-7xl text-green-500' />
-        </div>
-        <div className='p-4'>
-            <BiLogoPostgresql className='text-7xl text-blue-300' />
-        </div>
-        <div className='p-4'>
-            <RiTailwindCssFill className='text-7xl text-blue-500' />
-        </div>
-          <div className='p-4'>
-            <FaGitAlt className='text-7xl text-red-500' />
-        </div>
-        
-        <div className='p-4'>
-            <FaDocker className='text-7xl text-cyan-400' />
-        </div>
-      </div>
-    </div>
-  )
-}
+    <div className="pb-24">
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          initial="initial"
+          animate="animate"
+          variants={iconVariants(2.5)}
+        >
+          <RiReactjsLine className="text-7xl text-cyan-400 " />
+        </motion.div>
 
-export default Technologies
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(3)}
+        >
+          <TbBrandNextjs className="text-7xl animate" />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
+          <SiMongodb className="text-7xl text-green-800 " />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+        >
+
+          <FaNodeJs className="text-7xl text-green-500 " />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(4.5)}
+        >
+          <BiLogoPostgresql className="text-7xl text-blue-300" />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(5)}
+        >
+          <RiTailwindCssFill className="text-7xl text-blue-500" />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(2)}
+        >
+          <FaGitAlt className="text-7xl text-red-500" />
+        </motion.div>
+
+        <motion.div className="p-4"
+           initial="initial"
+          animate="animate"
+          variants={iconVariants(4)}
+        >
+          <FaDocker className="text-7xl text-cyan-400" />
+        </motion.div>
+
+      </motion.div>
+    </div>
+  );
+};
+
+export default Technologies;
